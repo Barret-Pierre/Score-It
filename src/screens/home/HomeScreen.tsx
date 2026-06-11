e760195fbac2d545de40d50cac09a1d581883700
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2026-06-12 00:13:00 +0200

    feat(home): fix image game into data

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -9,10 +9,10 @@ export type Game = {
 };
 
 const data = [
-  { id: '1', title: 'Flip 7', image: require('../../assets/flip-7.png') },
-  { id: '2', title: 'Odin', image: require('../../assets/odin.png') },
-  { id: '3', title: 'Skyjo', image: require('../../assets/skyjo.png') },
-  { id: '4', title: '6 qui prend', image: require('../../assets/6-qui-prend.png') },
+  { id: '1', title: 'Flip 7', image: require('../../../assets/flip-7.png') },
+  { id: '2', title: 'Odin', image: require('../../../assets/odin.png') },
+  { id: '3', title: 'Skyjo', image: require('../../../assets/skyjo.png') },
+  { id: '4', title: '6 qui prend', image: require('../../../assets/6-qui-prend.png') },
 ];
 
 export default function HomeScreen() {
