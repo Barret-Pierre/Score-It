ee082e5174c80864c9e23f6c8ca6bf3f0cef37c9
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2026-06-26 19:15:51 +0200

    feat(cards): add player cards and add player button on setup

--- src/screens/setup/SetupScreen.styles.ts
+++ src/screens/setup/SetupScreen.styles.ts
@@ -13,3 +13,33 @@ export const CustomSubtitle = styled.Text`
   line-height: ${({ theme }) => theme.semantic.typography.body.accent.lineHeight}px;
   color: ${({ theme }) => theme.semantic.text.accent};
 `;
+
+export const CardPlayer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.semantic.cards.gap}px;
+  background-color: ${({ theme }) => theme.semantic.cards.bg};
+  padding: ${({ theme }) => theme.semantic.cards.padding}px;
+  border-radius: ${({ theme }) => theme.semantic.cards.borderRadius}px;
+  border-color: ${({ theme }) => theme.semantic.cards.borderColor};
+  border-width: ${({ theme }) => theme.semantic.cards.borderWeight}px;
+`;
+
+export const PlayerName = styled.Text`
+  flex: 1;
+  color: ${({ theme }) => theme.semantic.text.placeholder};
+`;
+
+export const Button = styled.Pressable`
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.primitives.colors.transparent};
+  padding-horizontal: ${({ theme }) => theme.semantic.buttons.paddingHorizontal}px;
+  padding-vertical: ${({ theme }) => theme.semantic.buttons.paddingVertical}px;
+  border-radius: ${({ theme }) => theme.semantic.buttons.borderRadius}px;
+  border-color: ${({ theme }) => theme.primitives.colors.violet[500]};
+  border-width: ${({ theme }) => theme.primitives.border.weight[2]}px;
+  border-style: dashed;
+`;
--- src/screens/setup/SetupScreen.tsx
+++ src/screens/setup/SetupScreen.tsx
@@ -1,10 +1,23 @@
 import { useTheme } from '@/contexts/ThemeContext';
 import * as Styled from './SetupScreen.styles';
 import { ScreenHeader } from '@/components/ui';
-import { Text } from 'react-native';
+import { FlatList } from 'react-native';
+import { GripVertical, SquarePlus, Trash2 } from 'lucide-react-native';
+import { useCallback, useState } from 'react';
 
 export default function SetupScreen() {
-  const theme = useTheme();
+  const { theme } = useTheme();
+  const [players, setPlayers] = useState([{ id: '1', name: '' }]);
+
+  const gap = theme.semantic.screen.gap;
+
+  const onPress = useCallback(() => {
+    console.log('pressed');
+    setPlayers((prev) => {
+      console.log(prev.length);
+      return [...prev, { id: String(prev.length + 1), name: '' }];
+    });
+  }, []);
 
   return (
     <Styled.Container testID="setup-screen">
@@ -12,6 +25,23 @@ export default function SetupScreen() {
         title="Ajouter des joueurs"
         customSubtitle={<Styled.CustomSubtitle>2 joueurs minimum</Styled.CustomSubtitle>}
         testID="screen-header"
+      />
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ gap }}
+        renderItem={({ item }) => (
+          <Styled.CardPlayer>
+            <GripVertical color={theme.semantic.icon.primary} size={24} />
+            <Styled.PlayerName>{item.name || `Joueur ${item.id}`}</Styled.PlayerName>
+            <Trash2 color={theme.semantic.text.accent} />
+          </Styled.CardPlayer>
+        )}
+        ListFooterComponent={
+          <Styled.Button onPress={onPress}>
+            <SquarePlus color={theme.semantic.icon.enabled} size={24} />
+          </Styled.Button>
+        }
       />
     </Styled.Container>
   );
